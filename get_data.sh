5c1ef0e8f813b0e94ea983436ff8fa5de411f7c1
Alex C-G <alexcg@outlook.com>
2022-01-14 15:08:54 +0000

    feat: data getter

--- get_data.sh
+++ get_data.sh
@@ -1,1 +1,7 @@
-wget https://github.com/karoldvl/ESC-50/archive/master.zip
+mkdir -p data/esc-50
+wget https://github.com/karoldvl/ESC-50/archive/master.zip -P data/esc-50
+cd data/esc-50
+unzip master.zip
+rm -f master.zip
+mv master/ESC-50-master/audio .
+
